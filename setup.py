8c59322d2d42a392fb7cb264d6362a54efea64b0
Fabian Isensee <f.isensee@dkfz.de>
2024-02-19 17:07:28 +0100

    bump to v0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(name='dynamic_network_architectures',
       packages=find_namespace_packages(include=["dynamic_network_architectures", "dynamic_network_architectures.*"]),
-      version='0.3',
+      version='0.4',
       description='none',
       author='Fabian Isensee',
       author_email='f.isensee@dkfz.de',
