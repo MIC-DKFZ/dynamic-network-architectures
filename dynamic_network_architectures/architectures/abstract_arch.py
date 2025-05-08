bee2cd51568453451a6d90cc0f5b3efd6eb488ba
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2025-05-08 14:53:51 +0200

    Remove commented clutter

--- dynamic_network_architectures/architectures/abstract_arch.py
+++ dynamic_network_architectures/architectures/abstract_arch.py
@@ -4,8 +4,6 @@ from torch import nn
 import torch
 from dataclasses import dataclass
 
-stem_mode = Literal["random_init", "exact_match", "repeat_to_match"]
-
 
 class AbstractDynamicNetworkArchitectures(nn.Module):
 
@@ -19,53 +17,7 @@ class AbstractDynamicNetworkArchitectures(nn.Module):
         # Key to the weights that are dependent on the input channels.
         #   Can hold multiple weights (e.g. for bad weight mappings like in this repo >.<' )
         self.keys_to_in_proj: Sequence[str]
-
-    # def load_weights(
-    #     self,
-    #     to_be_loaded_state_dict: dict[str, torch.Tensor],
-    #     to_be_loaded_key_to_encoder: str,
-    #     to_be_loaded_key_to_stem: str,
-    #     to_be_loaded_stem_adapt_keys: Sequence[str],
-    #     load_encoder: bool,
-    #     load_stem: stem_mode,
-    # ):
-    #     """
-    #     Load weights into the network. This is a bit of a hack, but it works.
-    #     :param to_be_loaded_state_dict: The state dict to load from
-    #     :param load_encoder: Whether to load the encoder weights
-    #     :param load_stem: Whether to load the stem weight -- and in which way.
-    #     :return:
-    #     """
-
-    #     stem_part_of_encoder = self.key_to_stem.startswith(self.key_to_encoder)
-    #     current_state_dict = deepcopy(self.state_dict())
-
-    #     # -------------------------- Strip loaded state dict ------------------------- #
-    #     encoder_state_dict = {}
-    #     stem_state_dict = {}
-    #     for k, v in to_be_loaded_state_dict.items():
-    #         if k.startswith(to_be_loaded_key_to_encoder):
-    #             new_k = deepcopy(k)
-    #             new_k = new_k.replace(to_be_loaded_key_to_encoder, "")
-    #             if new_k.startswith("."):
-    #                 new_k = new_k[1:]
-    #             encoder_state_dict[k.replace(to_be_loaded_key_to_encoder, "")] = v
-    #         if k.startswith(to_be_loaded_key_to_stem):
-    #             new_k = deepcopy(k)
-    #             new_k = new_k.replace(to_be_loaded_key_to_stem, "")
-    #             if new_k.startswith("."):
-    #                 new_k = new_k[1:]
-    #             stem_state_dict[new_k] = v
-
-    #     # We keep the current state_dict to avoid
-    #     if load_encoder:
-    #         self.load_state_dict(to_be_loaded_state_dict, strict=False)
-    #         return
-
-    #     # Load the stem weights
-    #     for key in self.key_to_stem_weights:
-    #         if key in to_be_loaded_state_dict:
-    #             self.state_dict()[key].copy_(to_be_loaded_state_dict[key])
+        self.key_to_lpe: str | None = None  # LPE == Learnable Positional Embedding
 
 
 def test_submodules_loadable(model: AbstractDynamicNetworkArchitectures):
