f1d297f6343404fc974ba21045c1c0330b9d1a81
StePet99 <114655163+StePet99@users.noreply.github.com>
2025-06-11 15:26:10 +0200

    [ADD] added u2net modifying the original one in order to have more flexibility

--- dynamic_network_architectures/architectures/u2net.py
+++ dynamic_network_architectures/architectures/u2net.py
@@ -1,218 +1,108 @@
-from typing import Union, Type, List, Tuple
 import torch
 from torch import nn
-from torch.nn.modules.conv import _ConvNd
-from torch.nn.modules.dropout import _DropoutNd
-from dynamic_network_architectures.building_blocks.helper import convert_conv_op_to_dim
-from dynamic_network_architectures.building_blocks.simple_conv_blocks import ConvDropoutNormReLU
-from dynamic_network_architectures.initialization.weight_init import InitWeights_He, init_last_bn_before_add_to_0
+
+from dynamic_network_architectures.building_blocks.helper import convert_dim_to_conv_op, get_matching_pool_op
+from dynamic_network_architectures.building_blocks.RSUblock import RSU, _size_map, _upsample_like
+from dynamic_network_architectures.initialization.weight_init import InitWeights_He
+from dynamic_network_architectures.initialization.weight_init import init_last_bn_before_add_to_0
+
 
 __author__ = ["Stefano Petraccini"]
 __email__ = ["stefano.petraccini@studio.unibo.it"]
 
-class RSUBlock(nn.Module):
-    def __init__(
-        self,
-        in_ch,
-        mid_ch,
-        out_ch,
-        depth,
-        conv_op: Type[_ConvNd],
-        norm_op: Union[None, Type[nn.Module]] = None,
-        norm_op_kwargs: dict = None,
-        dropout_op: Union[None, Type[_DropoutNd]] = None,
-        dropout_op_kwargs: dict = None,
-        nonlin: Union[None, Type[nn.Module]] = None,
-        nonlin_kwargs: dict = None,
-        conv_bias: bool = False,
-    ):
-        super().__init__()
-        self.depth = depth
-        self.conv_op = conv_op
-        self.rebnconvin = ConvDropoutNormReLU(
-            conv_op, in_ch, out_ch, 3, 1, conv_bias, norm_op, norm_op_kwargs, dropout_op, dropout_op_kwargs, nonlin, nonlin_kwargs
-        )
-        pool_cls = nn.MaxPool3d if conv_op == nn.Conv3d else nn.MaxPool2d
-        self.pool = pool_cls(2, stride=2, ceil_mode=True)
-        # Encoder
-        self.encoders = nn.ModuleList()
-        self.encoders.append(
-            ConvDropoutNormReLU(
-                conv_op, out_ch, mid_ch, 3, 1, conv_bias, norm_op, norm_op_kwargs, dropout_op, dropout_op_kwargs, nonlin, nonlin_kwargs
-            )
-        )
-        for _ in range(1, depth):
-            self.encoders.append(
-                ConvDropoutNormReLU(
-                    conv_op, mid_ch, mid_ch, 3, 1, conv_bias, norm_op, norm_op_kwargs, dropout_op, dropout_op_kwargs, nonlin, nonlin_kwargs
-                )
-            )
-        # Decoder
-        self.decoders = nn.ModuleList()
-        for _ in range(depth-1):
-            self.decoders.append(
-                ConvDropoutNormReLU(
-                    conv_op, mid_ch * 2, mid_ch, 3, 1, conv_bias, norm_op, norm_op_kwargs, dropout_op, dropout_op_kwargs, nonlin, nonlin_kwargs
-                )
-            )
-        self.decoders.append(
-            ConvDropoutNormReLU(
-                conv_op, mid_ch + out_ch, out_ch, 3, 1, conv_bias, norm_op, norm_op_kwargs, dropout_op, dropout_op_kwargs, nonlin, nonlin_kwargs
-            )
-        )
+
+class U2NET(nn.Module):
+    def __init__(self, cfgs, dim, out_ch):
+        super(U2NET, self).__init__()
+        self.dim = dim
+        self.out_ch = out_ch
+        self._make_layers(cfgs, dim)
 
     def forward(self, x):
-        sizes = [x.shape[-3:] if x.dim() == 5 else x.shape[-2:]]
-        x_in = self.rebnconvin(x)
-        enc_feats = [self.encoders[0](x_in)]
-        x = self.pool(enc_feats[0])
-        sizes.append(x.shape[-3:] if x.dim() == 5 else x.shape[-2:])
-        # Encoder path
-        for i in range(1, self.depth):
-            enc = self.encoders[i](x)
-            enc_feats.append(enc)
-            if i < self.depth - 1:
-                x = self.pool(enc)
-                sizes.append(x.shape[-3:] if x.dim() == 5 else x.shape[-2:])
+        sizes = _size_map(x, self.height)
+        maps = []  # storage for maps
+
+        # side saliency map
+        def unet(x, height=1):
+            if height < 6:
+                x1 = getattr(self, f'stage{height}')(x)
+                x2 = unet(getattr(self, 'downsample')(x1), height + 1)
+                x = getattr(self, f'stage{height}d')(torch.cat((x2, x1), 1))
+                side(x, height)
+                return _upsample_like(x, self.dim, sizes[height - 1]) if height > 1 else x
             else:
-                x = enc
-        # Decoder path
-        for i in range(self.depth-1, 0, -1):
-            mode = 'trilinear' if x.dim() == 5 else 'bilinear'
-            x = nn.functional.interpolate(x, size=sizes[i], mode=mode, align_corners=False)
-            enc_up = nn.functional.interpolate(enc_feats[i-1], size=x.shape[-3:] if x.dim() == 5 else x.shape[-2:], mode=mode, align_corners=False)
-            x = torch.cat([x, enc_up], dim=1)
-            x = self.decoders[self.depth-1-i](x)
-        mode = 'trilinear' if x.dim() == 5 else 'bilinear'
-        x = nn.functional.interpolate(x, size=sizes[0], mode=mode, align_corners=False)
-        x_in_up = nn.functional.interpolate(x_in, size=x.shape[-3:] if x.dim() == 5 else x.shape[-2:], mode=mode, align_corners=False)
-        x = torch.cat([x, x_in_up], dim=1)
-        x = self.decoders[-1](x)
-        return x + x_in_up
+                x = getattr(self, f'stage{height}')(x)
+                side(x, height)
+                return _upsample_like(x, self.dim, sizes[height - 1])
+
+        def side(x, h):
+            # side output saliency map (before sigmoid)
+            x = getattr(self, f'side{h}')(x)
+            x = _upsample_like(x, self.dim, sizes[1])
+            maps.append(x)
+
+        def fuse():
+            # fuse saliency probability maps
+            maps.reverse()
+            x = torch.cat(maps, 1)
+            x = getattr(self, 'outconv')(x)
+            maps.insert(0, x)
+            return [torch.sigmoid(x) for x in maps]
+
+        unet(x)
+        maps = fuse()
+        return maps
+
+    def _make_layers(self, cfgs, dim):
+        self.height = int((len(cfgs) + 1) / 2)
+        self.add_module('downsample', get_matching_pool_op(dimension=dim, pool_type="max")(2, stride=2, ceil_mode=True))
+        for k, v in cfgs.items():
+            # build rsu block
+            self.add_module(k, RSU(v[0], dim, *v[1]))
+            if v[2] > 0:
+                # build side layer
+                self.add_module(f'side{v[0][-1]}', convert_dim_to_conv_op(dim)(v[2], self.out_ch, 3, padding=1))
+        # build fuse layer
+        self.add_module('outconv', convert_dim_to_conv_op(dim)(int(self.height * self.out_ch), self.out_ch, 1))
+      
+@staticmethod
+def initialize(module):
+    InitWeights_He(1e-2)(module)
+    init_last_bn_before_add_to_0(module)
+
+def U2NET_full(dim):
+    full = {
+        # cfgs for building RSUs and sides
+        # {stage : [name, (height(L), in_ch, mid_ch, out_ch, dilated), side]}
+        'stage1': ['En_1', (7, 3, 32, 64), -1],
+        'stage2': ['En_2', (6, 64, 32, 128), -1],
+        'stage3': ['En_3', (5, 128, 64, 256), -1],
+        'stage4': ['En_4', (4, 256, 128, 512), -1],
+        'stage5': ['En_5', (4, 512, 256, 512, True), -1],
+        'stage6': ['En_6', (4, 512, 256, 512, True), 512],
+        'stage5d': ['De_5', (4, 1024, 256, 512, True), 512],
+        'stage4d': ['De_4', (4, 1024, 128, 256), 256],
+        'stage3d': ['De_3', (5, 512, 64, 128), 128],
+        'stage2d': ['De_2', (6, 256, 32, 64), 64],
+        'stage1d': ['De_1', (7, 128, 16, 64), 64],
+    }
+    return U2NET(full, dim, out_ch=1)
 
 
-class U2Net(nn.Module):
-    def __init__(
-        self,
-        input_channels: int,
-        n_stages: int,
-        features_per_stage: Union[int, List[int], Tuple[int, ...]],
-        mid_channels: Union[int, List[int], Tuple[int, ...]],
-        num_classes: int,
-        rsu_depths: Union[int, List[int], Tuple[int, ...]],
-        conv_op: Type[_ConvNd],
-        conv_bias: bool = False,
-        norm_op: Union[None, Type[nn.Module]] = None,
-        norm_op_kwargs: dict = None,
-        dropout_op: Union[None, Type[_DropoutNd]] = None,
-        dropout_op_kwargs: dict = None,
-        nonlin: Union[None, Type[nn.Module]] = None,
-        nonlin_kwargs: dict = None,
-        deep_supervision: bool = False,
-    ):
-        super().__init__()
-        if isinstance(features_per_stage, int):
-            features_per_stage = [features_per_stage] * n_stages
-        if isinstance(mid_channels, int):
-            mid_channels = [mid_channels] * n_stages
-        if isinstance(rsu_depths, int):
-            rsu_depths = [rsu_depths] * n_stages
-
-        self.deep_supervision = deep_supervision
-        self.conv_op = conv_op
-
-        # Encoder
-        self.encoder = nn.ModuleList()
-        in_ch = input_channels
-        for i in range(n_stages):
-            self.encoder.append(
-                RSUBlock(
-                    in_ch, mid_channels[i], features_per_stage[i], rsu_depths[i],
-                    conv_op, norm_op, norm_op_kwargs, dropout_op, dropout_op_kwargs, nonlin, nonlin_kwargs, conv_bias
-                )
-            )
-            in_ch = features_per_stage[i]
-
-        # Decoder
-        self.decoder = nn.ModuleList()
-        for i in reversed(range(1, n_stages)):
-            self.decoder.append(
-                RSUBlock(
-                    features_per_stage[i] + features_per_stage[i-1],
-                    mid_channels[i-1],
-                    features_per_stage[i-1],
-                    rsu_depths[i-1],
-                    conv_op, norm_op, norm_op_kwargs, dropout_op, dropout_op_kwargs, nonlin, nonlin_kwargs, conv_bias
-                )
-            )
-
-        self.final = conv_op(features_per_stage[0], num_classes, 1)
-
-    def forward(self, x):
-        skips = []
-        sizes = []
-        for enc in self.encoder:
-            x = enc(x)
-            skips.append(x)
-            sizes.append(x.shape[-3:] if x.dim() == 5 else x.shape[-2:])
-            pool = nn.functional.max_pool3d if x.dim() == 5 else nn.functional.max_pool2d
-            x = pool(x, 2, 2, ceil_mode=True)
-        x = skips.pop()
-        for idx, dec in enumerate(self.decoder):
-            mode = 'trilinear' if x.dim() == 5 else 'bilinear'
-            x = nn.functional.interpolate(x, size=sizes[-(idx+2)], mode=mode, align_corners=False)
-            x = torch.cat([x, skips.pop()], dim=1)
-            x = dec(x)
-        x = self.final(x)
-        return x
-
-    @staticmethod
-    def initialize(module):
-        InitWeights_He(1e-2)(module)
-        init_last_bn_before_add_to_0(module)
-
-
-if __name__ == "__main__":
-    # Esempio 2D
-    model2d = U2Net(
-        input_channels=3,
-        n_stages=4,
-        features_per_stage=[64, 128, 256, 512],
-        mid_channels=[32, 64, 128, 256],
-        num_classes=2,
-        rsu_depths=[2, 3, 4, 4],
-        conv_op=nn.Conv2d,
-        conv_bias=False,
-        norm_op=nn.BatchNorm2d,
-        norm_op_kwargs=None,
-        dropout_op=None,
-        dropout_op_kwargs=None,
-        nonlin=nn.ReLU,
-        nonlin_kwargs=None,
-        deep_supervision=True,
-    )
-    x2d = torch.randn(1, 3, 256, 256)
-    out2d = model2d(x2d)
-    print("2D Output shape:", out2d.shape)
-
-    # Esempio 3D
-    model3d = U2Net(
-        input_channels=1,
-        n_stages=4,
-        features_per_stage=[16, 32, 64, 128],
-        mid_channels=[8, 16, 32, 64],
-        num_classes=2,
-        rsu_depths=[2, 3, 4, 4],
-        conv_op=nn.Conv3d,
-        conv_bias=False,
-        norm_op=nn.BatchNorm3d,
-        norm_op_kwargs=None,
-        dropout_op=None,
-        dropout_op_kwargs=None,
-        nonlin=nn.ReLU,
-        nonlin_kwargs=None,
-        deep_supervision=False,
-    )
-    x3d = torch.randn(1, 1, 32, 64, 64)
-    out3d = model3d(x3d)
-    print("3D Output shape:", out3d.shape)
+def U2NET_lite(dim):
+    lite = {
+        # cfgs for building RSUs and sides
+        # {stage : [name, (height(L), in_ch, mid_ch, out_ch, dilated), side]}
+        'stage1': ['En_1', (7, 3, 16, 64), -1],
+        'stage2': ['En_2', (6, 64, 16, 64), -1],
+        'stage3': ['En_3', (5, 64, 16, 64), -1],
+        'stage4': ['En_4', (4, 64, 16, 64), -1],
+        'stage5': ['En_5', (4, 64, 16, 64, True), -1],
+        'stage6': ['En_6', (4, 64, 16, 64, True), 64],
+        'stage5d': ['De_5', (4, 128, 16, 64, True), 64],
+        'stage4d': ['De_4', (4, 128, 16, 64), 64],
+        'stage3d': ['De_3', (5, 128, 16, 64), 64],
+        'stage2d': ['De_2', (6, 128, 16, 64), 64],
+        'stage1d': ['De_1', (7, 128, 16, 64), 64],
+    }
+    return U2NET(lite, dim, out_ch=1)
