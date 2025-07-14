e2fde284f81dff01d0f3af2058d89a40413f92e3
StePet99 <114655163+StePet99@users.noreply.github.com>
2025-07-14 10:13:42 +0200

    [ENH] implemented 3D.

--- dynamic_network_architectures/architectures/2u2net.py
+++ dynamic_network_architectures/architectures/2u2net.py
@@ -1,41 +1,48 @@
+from typing import Union, Type, List, Tuple
+import torch
 from torch import nn
+from torch.nn.modules.conv import _ConvNd
+from torch.nn.modules.dropout import _DropoutNd
+
 from dynamic_network_architectures.architectures.abstract_arch import AbstractDynamicNetworkArchitectures
 from dynamic_network_architectures.building_blocks.RSU2 import RSUEncoder, RSUDecoder
 from dynamic_network_architectures.building_blocks.helper import convert_conv_op_to_dim
-from typing import Union, List, Tuple, Type
 
-from torch.nn.modules.dropout import _DropoutNd
+__author__ = ["Stefano Petraccini"]
+__email__ = ["stefano.petraccini@studio.unibo.it"]
 
-from dynamic_network_architectures.building_blocks.simple_conv_blocks import StackedConvBlocks
-from dynamic_network_architectures.building_blocks.helper import get_matching_convtransp
-
-#TODO: va fixata la deep supervision
 class U2Net(AbstractDynamicNetworkArchitectures):
     def __init__(
         self,
         input_channels: int,
-        num_classes: int, 
         n_stages: int,
-        features_per_stage: list,
-        conv_op,
-        kernel_sizes: list,
-        strides: list,
-        n_conv_per_stage: int,
-        conv_bias: bool,
-        norm_op,
-        norm_op_kwargs: dict,
-        dropout_op,
-        dropout_op_kwargs: dict,
-        nonlin, ########## TODO: ideally we want to be able to define different nonlinearities for both RSUBlock and "general" nonlinearity (the one used by nnUNet)
-        nonlin_kwargs: dict,
-        deep_supervision: bool = True,
+        features_per_stage: Union[int, List[int], Tuple[int, ...]],
+        conv_op: Type[_ConvNd],
+        kernel_sizes: Union[int, List[int], Tuple[int, ...]],
+        strides: Union[int, List[int], Tuple[int, ...]],
+        n_conv_per_stage: Union[int, List[int], Tuple[int, ...]],
+        num_classes: int,
+        conv_bias: bool = False,
+        norm_op: Union[None, Type[nn.Module]] = None,
+        norm_op_kwargs: dict = None,
+        dropout_op: Union[None, Type[_DropoutNd]] = None,
+        dropout_op_kwargs: dict = None,
+        nonlin: Union[None, Type[torch.nn.Module]] = None,
+        nonlin_kwargs: dict = None,
+        blocks_nonlin: Union[None, Type[torch.nn.Module]] = None,
+        blocks_nonlin_kwargs: dict = None,
+        deep_supervision: bool = False,
         return_skips: bool = True,
         nonlin_first: bool = False,
         pool: str = "max",
-        depth_per_stage: list = None,
-        **kwargs
+        depth_per_stage: Union[int, List[int], Tuple[int, ...]] = None,
     ):
         super().__init__()
+        # If we don't have a specific nonlinearity for blocks, use the default nonlin
+        if blocks_nonlin is None:
+            blocks_nonlin = nonlin
+            blocks_nonlin_kwargs = nonlin_kwargs 
+
         self.deep_supervision = deep_supervision
         self.encoder = RSUEncoder(
             input_channels,
@@ -50,19 +57,23 @@ class U2Net(AbstractDynamicNetworkArchitectures):
             norm_op_kwargs,
             dropout_op,
             dropout_op_kwargs,
-            nonlin,
-            nonlin_kwargs,
+            blocks_nonlin,  # Passa blocks_nonlin
+            blocks_nonlin_kwargs,
             return_skips,
             nonlin_first,
             pool,
-            depth_per_stage = depth_per_stage
+            depth_per_stage=depth_per_stage
         )
-        self.decoder = RSUDecoder(self.encoder, num_classes, deep_supervision) 
-        
+        self.decoder = RSUDecoder(
+            self.encoder,
+            num_classes,
+            deep_supervision = deep_supervision
+        )
+
     def forward(self, x):
         skips = self.encoder(x)
         return self.decoder(skips)
-    
+
     def compute_conv_feature_map_size(self, input_size):
         assert len(input_size) == convert_conv_op_to_dim(self.encoder.conv_op), (
             "just give the image size without color/feature channels or "
--- dynamic_network_architectures/building_blocks/RSU2.py
+++ dynamic_network_architectures/building_blocks/RSU2.py
@@ -2,98 +2,161 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 import numpy as np
+from typing import Union, List, Tuple, Type, Optional
 
-from dynamic_network_architectures.building_blocks.helper import maybe_convert_scalar_to_list, get_matching_pool_op, convert_dim_to_conv_op, get_matching_convtransp
-#TODO: utilizzare le varie funzioni modulari di helper.py tipo _get_matching_pool_op, _from_conv_get_dim (o come si chiamano)
+from dynamic_network_architectures.building_blocks.helper import (
+    maybe_convert_scalar_to_list,
+    get_matching_pool_op,
+    convert_conv_op_to_dim,
+)
+
+__author__ = ["Stefano Petraccini"]
+__email__ = ["stefano.petraccini@studio.unibo.it"]
+
 class RSUBlock(nn.Module):
-    def __init__(self,
-                in_ch,
-                out_ch,
-                mid_ch,
-                depth = 4,
-                conv_op = nn.Conv2d,
-                kernel_size = 3,
-                stride = 1,
-                bias = True,
-                nonlin = nn.ReLU, #TODO: voglio forse poter distinguere tra la nonlnearity generale e quella usata nei blocchi RSU come fa l'architettura originale
-                norm_op = nn.BatchNorm2d
-                ): #TODO: implementare un nonlin_first che permette di mettere la non linearità prima della normalizzazione
-        
+    def __init__(
+        self,
+        in_ch: int,
+        out_ch: int,
+        mid_ch: Optional[int],
+        depth: int = 4,
+        conv_op: Type[nn.Module] = nn.Conv2d,
+        kernel_size: Union[int, Tuple[int, ...], List[int]] = 3,
+        stride: Union[int, Tuple[int, ...], List[int]] = 1,
+        bias: bool = True,
+        nonlin: Optional[Type[nn.Module]] = nn.ReLU,
+        norm_op: Optional[Type[nn.Module]] = nn.BatchNorm2d,
+        norm_op_kwargs: Optional[dict] = None,
+        dropout_op: Optional[Type[nn.Module]] = None,
+        dropout_op_kwargs: Optional[dict] = None,
+        nonlin_kwargs: Optional[dict] = None,
+        pool: str = "max",
+        nonlin_first: bool = False
+    ):
         super().__init__()
-        # Parameters
         self.depth = depth
+        self.nonlin_first = nonlin_first
         self.encoders = nn.ModuleList()
         self.pools = nn.ModuleList()
         self.decoders = nn.ModuleList()
-        self.conv_in = conv_op(in_ch, out_ch, kernel_size, stride, padding=kernel_size//2, bias=bias)
-        self.nonlin = nonlin()
-        self.norm = norm_op(out_ch)
+
+        # Determina la dimensione (2D o 3D)
+        dim = convert_conv_op_to_dim(conv_op)
+        kernel_size = maybe_convert_scalar_to_list(conv_op, kernel_size)
+        stride = maybe_convert_scalar_to_list(conv_op, stride)
+        padding = [k // 2 for k in kernel_size]
+
+        # Funzioni di attivazione, normalizzazione e dropout
+        nonlin_kwargs = {} if nonlin_kwargs is None else nonlin_kwargs
+        norm_op_kwargs = {} if norm_op_kwargs is None else norm_op_kwargs
+        self.nonlin = nonlin(**nonlin_kwargs) if nonlin else nn.Identity()
+        self.dropout = dropout_op(**(dropout_op_kwargs or {})) if dropout_op else nn.Identity()
+
+        # Ingresso
+        self.conv_in = conv_op(in_ch, out_ch, kernel_size, stride, padding=padding, bias=bias)
+        self.norm_in = norm_op(out_ch, **norm_op_kwargs) if norm_op else nn.Identity()
 
         # Encoder path 
+        pool_op = get_matching_pool_op(conv_op, pool_type=pool)
+        self.enc_norms = nn.ModuleList()
         for i in range(depth):
-            self.encoders.append(conv_op(out_ch if i == 0 else mid_ch, mid_ch, kernel_size, stride, padding=kernel_size//2, bias=bias))
-            self.pools.append(nn.MaxPool2d(2, 2))
+            self.encoders.append(
+                conv_op(out_ch if i == 0 else mid_ch, mid_ch, kernel_size, stride, padding=padding, bias=bias)
+            )
+            self.enc_norms.append(
+                norm_op(mid_ch, **norm_op_kwargs) if norm_op else nn.Identity()
+            )
+            self.pools.append(pool_op(kernel_size=2, stride=2))
 
         # Bottom
-        self.bottom = conv_op(mid_ch, mid_ch, kernel_size, stride, padding=kernel_size//2, bias=bias)
+        self.bottom = conv_op(mid_ch, mid_ch, kernel_size, stride, padding=padding, bias=bias)
+        self.norm_bottom = norm_op(mid_ch, **norm_op_kwargs) if norm_op else nn.Identity()
 
-        # Decoder path  TODO: capire bene come si comporta, lo ha modificato copilot per risolvere errore di dimensione
+        # Decoder path
+        self.dec_norms = nn.ModuleList()
         for i in range(depth):
-            # L'ultimo skip è x_in (out_ch), gli altri sono mid_ch
             skip_ch = out_ch if i == depth - 1 else mid_ch
             in_ch_dec = mid_ch + skip_ch
             out_ch_dec = mid_ch if i < depth - 1 else out_ch
             self.decoders.append(
-                conv_op(in_ch_dec, out_ch_dec, kernel_size, stride, padding=kernel_size//2, bias=bias)
+                conv_op(in_ch_dec, out_ch_dec, kernel_size, stride, padding=padding, bias=bias)
+            )
+            self.dec_norms.append(
+                norm_op(out_ch_dec, **norm_op_kwargs) if norm_op else nn.Identity()
             )
 
-    def forward(self, x):
-        x_in = self.nonlin(self.norm(self.conv_in(x))) #This is the original REBNCONV
+    def _apply_block(self, conv, norm, nonlin, x):
+        x = conv(x)
+        # Controllo robusto per InstanceNorm2d e InstanceNorm3d
+        is_instancenorm = isinstance(norm, (nn.InstanceNorm2d, nn.InstanceNorm3d))
+        # Le dimensioni spaziali sono tutte dopo la seconda (batch, canali, ...)
+        spatial_dims = x.shape[2:]
+        too_small_for_instancenorm = np.prod(spatial_dims) <= 1
+
+        if not isinstance(norm, nn.Identity) and not (is_instancenorm and too_small_for_instancenorm):
+            if self.nonlin_first:
+                x = nonlin(x)
+                x = norm(x)
+            else:
+                x = norm(x)
+                x = nonlin(x)
+        else:
+            # Se saltiamo la normalizzazione, applichiamo solo l'attivazione
+            x = nonlin(x)
+    
+        return x
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x_in = self._apply_block(self.conv_in, self.norm_in, self.nonlin, x)
+        x_in = self.dropout(x_in)
         enc_feats = [x_in]
         xi = x_in
         # Encoder
-        for enc, pool in zip(self.encoders, self.pools):
-            xi = self.nonlin(enc(xi))
+        for enc, norm, pool in zip(self.encoders, self.enc_norms, self.pools):
+            xi = self._apply_block(enc, norm, self.nonlin, xi)
+            xi = self.dropout(xi)
             enc_feats.append(xi)
-            # ONLY IF DIMENSION > 1x1 
-            ##### questo risolve degli errori di dimensione quando si usa il pooling
-            if xi.shape[2] > 1 and xi.shape[3] > 1:
+            if all(s > 1 for s in xi.shape[2:]):
                 xi = pool(xi)
         # Bottleneck
-        xb = self.nonlin(self.bottom(xi)) #
+        xb = self._apply_block(self.bottom, self.norm_bottom, self.nonlin, xi)
+        xb = self.dropout(xb)
         # Decoder
         xu = xb
-        for i, dec in enumerate(self.decoders):
+        for i, (dec, norm) in enumerate(zip(self.decoders, self.dec_norms)):
             skip = enc_feats[-(i+2)]
-            xu = F.interpolate(xu, size=skip.shape[2:], mode='bilinear', align_corners=False)
+            mode = 'trilinear' if xu.dim() == 5 else 'bilinear'
+            xu = F.interpolate(xu, size=skip.shape[2:], mode=mode, align_corners=False)
             xu = torch.cat([xu, skip], dim=1)
-            xu = self.nonlin(dec(xu))
+            xu = self._apply_block(dec, norm, self.nonlin, xu)
+            xu = self.dropout(xu)
         if xu.shape[2:] != x_in.shape[2:]:
-            xu = F.interpolate(xu, size=x_in.shape[2:], mode='bilinear', align_corners=False)
+            mode = 'trilinear' if xu.dim() == 5 else 'bilinear'
+            xu = F.interpolate(xu, size=x_in.shape[2:], mode=mode, align_corners=False)
         return xu + x_in
-
 
 class RSUEncoder(nn.Module):
     def __init__(
         self,
-        input_channels,
-        n_stages,
-        features_per_stage,
-        conv_op,
-        kernel_sizes,
-        strides,
-        n_conv_per_stage,
-        conv_bias,
-        norm_op,
-        norm_op_kwargs,
-        dropout_op,
-        dropout_op_kwargs,
-        nonlin,
-        nonlin_kwargs,
-        return_skips = True,
-        nonlin_first = False,
-        pool = "max",
-        depth_per_stage = None
+        input_channels: int,
+        n_stages: int,
+        features_per_stage: List[int],
+        conv_op: Type[nn.Module],
+        kernel_sizes: List[Union[int, Tuple[int, ...], List[int]]],
+        strides: List[Union[int, Tuple[int, ...], List[int]]],
+        n_conv_per_stage: Union[int, List[int], Tuple[int, ...]],
+        conv_bias: bool,
+        norm_op: Optional[Type[nn.Module]],
+        norm_op_kwargs: Optional[dict],
+        dropout_op: Optional[Type[nn.Module]],
+        dropout_op_kwargs: Optional[dict],
+        nonlin: Optional[Type[nn.Module]],
+        nonlin_kwargs: Optional[dict],
+        return_skips: bool = True,
+        nonlin_first: bool = False,
+        pool: str = "max",
+        depth_per_stage: Optional[List[int]] = None,
+        blocks_nonlin: Optional[Type[nn.Module]] = None,
     ):
         super().__init__()
         self.conv_op = conv_op
@@ -107,12 +170,14 @@ class RSUEncoder(nn.Module):
         self.kernel_sizes = kernel_sizes
         self.return_skips = return_skips
         self.features_per_stage = features_per_stage
-        self.stages = nn.ModuleList()
         self.depth_per_stage = depth_per_stage if depth_per_stage is not None else [4] * n_stages
         self.bias = conv_bias
+        self.blocks_nonlin = blocks_nonlin if blocks_nonlin is not None else nonlin
+        self.nonlin_first = nonlin_first
+        self.stages = nn.ModuleList()
         prev_ch = input_channels
         for i in range(n_stages):
-            depth = depth_per_stage[i] if depth_per_stage is not None else 4
+            depth = self.depth_per_stage[i]
             mid_ch = features_per_stage[i] // 2
             self.stages.append(
                 RSUBlock(
@@ -121,16 +186,22 @@ class RSUEncoder(nn.Module):
                     mid_ch=mid_ch,
                     depth=depth,
                     conv_op=conv_op,
-                    kernel_size=kernel_sizes[i][0] if isinstance(kernel_sizes[i], (list, tuple)) else kernel_sizes[i],
-                    stride=strides[i][0] if isinstance(strides[i], (list, tuple)) else strides[i],
+                    kernel_size=kernel_sizes[i],
+                    stride=strides[i],
                     bias=conv_bias,
-                    nonlin=nonlin,
-                    norm_op=norm_op
+                    nonlin=self.blocks_nonlin,
+                    norm_op=norm_op,
+                    norm_op_kwargs=norm_op_kwargs,
+                    dropout_op=dropout_op,
+                    dropout_op_kwargs=dropout_op_kwargs,
+                    nonlin_kwargs=nonlin_kwargs,
+                    pool=pool,
+                    nonlin_first=nonlin_first
                 )
             )
             prev_ch = features_per_stage[i]
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> Union[List[torch.Tensor], torch.Tensor]:
         skips = []
         for stage in self.stages:
             x = stage(x)
@@ -139,7 +210,7 @@ class RSUEncoder(nn.Module):
             return skips
         return x
     
-    def compute_conv_feature_map_size(self, input_size):
+    def compute_conv_feature_map_size(self, input_size: List[int]) -> int:
         output = np.int64(0)
         for s in range(len(self.stages)):
             if isinstance(self.stages[s], nn.Sequential):
@@ -151,19 +222,23 @@ class RSUEncoder(nn.Module):
             input_size = [i // j for i, j in zip(input_size, self.strides[s])]
         return output
 
-
 class RSUDecoder(nn.Module):
-    def __init__(self, 
-                encoder,
-                num_classes,
-                deep_supervision = False):
-        
+    def __init__(
+        self,
+        encoder: RSUEncoder,
+        num_classes: int,
+        deep_supervision: bool = False,
+        blocks_nonlin: Optional[Type[nn.Module]] = None,
+        nonlin_first: bool = False
+    ):
         super().__init__()
         self.stages = nn.ModuleList()
         self.deep_supervision = deep_supervision
         self.num_classes = num_classes
-        self.encoder = encoder  # Store the encoder for compute_conv_feature_map_size
-        
+        self.encoder = encoder
+        self.blocks_nonlin = blocks_nonlin if blocks_nonlin is not None else encoder.blocks_nonlin
+        self.nonlin_first = nonlin_first
+
         features_per_stage = encoder.features_per_stage
         n_stages = len(features_per_stage)
         for i in range(n_stages-1, 0, -1):
@@ -174,11 +249,16 @@ class RSUDecoder(nn.Module):
                     mid_ch = features_per_stage[i-1]//2,
                     depth = encoder.depth_per_stage[i-1],
                     conv_op = encoder.conv_op,
-                    kernel_size = encoder.kernel_sizes[i-1][0] if isinstance(encoder.kernel_sizes[i-1], (list, tuple)) else encoder.kernel_sizes[i-1],
+                    kernel_size = encoder.kernel_sizes[i-1],
                     stride = 1,
                     bias = encoder.bias,
-                    nonlin = encoder.nonlin,
-                    norm_op = encoder.norm_op
+                    nonlin = self.blocks_nonlin,
+                    norm_op = encoder.norm_op,
+                    norm_op_kwargs = encoder.norm_op_kwargs,
+                    dropout_op = encoder.dropout_op,
+                    dropout_op_kwargs = encoder.dropout_op_kwargs,
+                    nonlin_kwargs = encoder.nonlin_kwargs,
+                    nonlin_first = nonlin_first
                 )
             )
         # Segmentation heads: one per output for deep supervision, or just one if not
@@ -187,11 +267,12 @@ class RSUDecoder(nn.Module):
             for i in range(n_stages-1, 0, -1)
         ])
 
-    def forward(self, skips):
+    def forward(self, skips: List[torch.Tensor]) -> Union[torch.Tensor, List[torch.Tensor]]:
         x = skips[-1]
         outputs = []
         for i, stage in enumerate(self.stages):
-            x = F.interpolate(x, size=skips[-(i+2)].shape[2:], mode='bilinear', align_corners=False)
+            mode = 'trilinear' if x.dim() == 5 else 'bilinear'
+            x = F.interpolate(x, size=skips[-(i+2)].shape[2:], mode=mode, align_corners=False)
             x = torch.cat([x, skips[-(i+2)]], dim=1)
             x = stage(x)
             # Apply segmentation head to get num_classes channels
@@ -204,31 +285,17 @@ class RSUDecoder(nn.Module):
         else:
             r = outputs
         return r
-    def compute_conv_feature_map_size(self, input_size):
-        """
-        IMPORTANT: input_size is the input_size of the encoder!
-        :param input_size:
-        :return:
-        """
-        # first we need to compute the skip sizes. Skip bottleneck because all output feature maps of our ops will at
-        # least have the size of the skip above that (therefore -1)
+
+    def compute_conv_feature_map_size(self, input_size: List[int]) -> int:
         skip_sizes = []
         for s in range(len(self.encoder.strides) - 1):
             skip_sizes.append([i // j for i, j in zip(input_size, self.encoder.strides[s])])
             input_size = skip_sizes[-1]
-        # print(skip_sizes)
-
         assert len(skip_sizes) == len(self.stages)
-
-        # our ops are the other way around, so let's match things up
         output = np.int64(0)
         for s in range(len(self.stages)):
-            # print(skip_sizes[-(s+1)], self.encoder.output_channels[-(s+2)])
-            # conv blocks
             output += self.stages[s].compute_conv_feature_map_size(skip_sizes[-(s+1)])
-            # trans conv
             output += np.prod([self.encoder.output_channels[-(s+2)], *skip_sizes[-(s+1)]], dtype=np.int64)
-            # segmentation
             if self.deep_supervision or (s == (len(self.stages) - 1)):
                 output += np.prod([self.num_classes, *skip_sizes[-(s+1)]], dtype=np.int64)
         return output
