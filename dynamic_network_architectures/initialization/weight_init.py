5f378316ff19d6c83567a9be3b149208e603b946
Sam Bond-Taylor <18750296+samb-t@users.noreply.github.com>
2025-08-15 13:26:08 +0100

    Remove parameter overwrite when initialising
    When loading checkpoints with `transformers`, they patch torch's init functions to not do anything. This breaks these inits as it effectively deletes the parameters and then the checkpoint can't be loaded.

--- dynamic_network_architectures/initialization/weight_init.py
+++ dynamic_network_architectures/initialization/weight_init.py
@@ -9,9 +9,9 @@ class InitWeights_He(object):
 
     def __call__(self, module):
         if isinstance(module, nn.Conv3d) or isinstance(module, nn.Conv2d) or isinstance(module, nn.ConvTranspose2d) or isinstance(module, nn.ConvTranspose3d):
-            module.weight = nn.init.kaiming_normal_(module.weight, a=self.neg_slope)
+            nn.init.kaiming_normal_(module.weight, a=self.neg_slope)
             if module.bias is not None:
-                module.bias = nn.init.constant_(module.bias, 0)
+                nn.init.constant_(module.bias, 0)
 
 
 class InitWeights_XavierUniform(object):
@@ -20,15 +20,15 @@ class InitWeights_XavierUniform(object):
 
     def __call__(self, module):
         if isinstance(module, nn.Conv3d) or isinstance(module, nn.Conv2d) or isinstance(module, nn.ConvTranspose2d) or isinstance(module, nn.ConvTranspose3d):
-            module.weight = nn.init.xavier_uniform_(module.weight, self.gain)
+            nn.init.xavier_uniform_(module.weight, self.gain)
             if module.bias is not None:
-                module.bias = nn.init.constant_(module.bias, 0)
+                nn.init.constant_(module.bias, 0)
 
 
 def init_last_bn_before_add_to_0(module):
     if isinstance(module, BasicBlockD):
-        module.conv2.norm.weight = nn.init.constant_(module.conv2.norm.weight, 0)
-        module.conv2.norm.bias = nn.init.constant_(module.conv2.norm.bias, 0)
+        nn.init.constant_(module.conv2.norm.weight, 0)
+        nn.init.constant_(module.conv2.norm.bias, 0)
     if isinstance(module, BottleneckD):
-        module.conv3.norm.weight = nn.init.constant_(module.conv3.norm.weight, 0)
-        module.conv3.norm.bias = nn.init.constant_(module.conv3.norm.bias, 0)
+        nn.init.constant_(module.conv3.norm.weight, 0)
+        nn.init.constant_(module.conv3.norm.bias, 0)
