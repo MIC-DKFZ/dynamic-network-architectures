314c814417317eb5ac2c1d0a8c8a7d14c4b2591c
Fabian Isensee <f.isensee@dkfz.de>
2026-01-23 09:54:41 +0100

    restrict timm version (#20), use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,18 +1,4 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup
 
-setup(
-    name="dynamic_network_architectures",
-    packages=find_namespace_packages(include=["dynamic_network_architectures", "dynamic_network_architectures.*"]),
-    version="0.4.2",
-    description="none",
-    author="Fabian Isensee",
-    author_email="f.isensee@dkfz.de",
-    license="private",
-    install_requires=[
-        "torch>=1.6.0a",
-        "numpy",
-        "timm",
-        "einops",
-    ],
-    zip_safe=False,
-)
+if __name__ == "__main__":
+    setup()
